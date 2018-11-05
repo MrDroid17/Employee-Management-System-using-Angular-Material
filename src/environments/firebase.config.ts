a934622c2c1346dfae731a8a45e9549802aedf35
Sobhit Kumar <sobhit2010@gmail.com>
2018-11-06 00:03:36 +0530

    managing folder

---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: 'AIzaSyDqWWOHnhE4zjy_aYHXA3rRXJEAc7zkjV0',
+    authDomain: 'universal-translator-50f74.firebaseapp.com',
+    databaseURL: 'https://universal-translator-50f74.firebaseio.com',
+    projectId: 'universal-translator-50f74',
+    storageBucket: 'universal-translator-50f74.appspot.com',
+    messagingSenderId: '734904081513'
+};
